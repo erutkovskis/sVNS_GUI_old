b30582b18a99bbdda005b4791af991da5e4817b3
erutkovskis <edvards.rutkovskis@gmail.com>
2022-12-12 14:18:14 +0000

    Checkpoint 3. Added test pyserial communications: button send their values to the serial interface.

--- gui_1.py
+++ gui_1.py
@@ -1,16 +1,30 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+import serial
+import time
+
+arduino = serial.Serial(port='',baudrate=115200,timeout=0.1)
 
 # Future button converters
 def PW_button_send(number):
     '''Convert the button value to hex and prints on the terminal'''
-    hex_value = hex(number)
-    print(hex_value)
+#    hex_value = hex(number)
+#    print(hex_value)
+    arduino.write(bytes(number,'utf-8'))
+    time.sleep(0.05)
+
 
 def PF_button_send(number):
     '''Convert the button value to hex and prints on the terminal'''
-    hex_value = hex(number)
-    print(hex_value)
+#    hex_value = hex(number)
+#    print(hex_value)
+    arduino.write(bytes(number,'utf-8'))
+    time.sleep(0.05)
+
+def ser_mon_display():
+    time.sleep(0.05)
+    print(arduino.readline())
+
 
 window = tk.Tk()
 
@@ -20,6 +34,7 @@ frame_pf = tk.Frame(relief=tk.RIDGE,borderwidth=5)
 frame_stimon = tk.Frame(relief=tk.RIDGE,borderwidth=5)
 frame_curampl = tk.Frame(relief=tk.RIDGE,borderwidth=5)
 frame_onoff = tk.Frame(relief=tk.RIDGE,borderwidth=5)
+frame_serial_mon = tk.Frame(relief=tk.RIDGE,borderwidth=5)
 
 # Make frames as grid
 frame_pw.grid(row=0,column=0,padx=5,pady=5)
@@ -27,6 +42,7 @@ frame_pf.grid(row=0,column=1,padx=5,pady=5)
 frame_stimon.grid(row=0,column=2,padx=5,pady=5)
 frame_curampl.grid(row=0,column=3,padx=5,pady=5)
 frame_onoff.grid(row=0,column=4,padx=5,pady=5)
+frame_serial_mon.grid(row=1,column=0, columnspan=5,padx=5,pady=5)
 
 # Define labels
     # Frame labels
@@ -50,6 +66,8 @@ hz_lbl = tk.Label(text="Hz")
     # current label
 uA_lbl = tk.Label(text="uA")
 mA_lbl = tk.Label(master=frame_curampl, text="mA")
+    # Serial monitor label
+serial_mon_lbl = tk.Label(master=frame_serial_mon,text="Serial Monitor")
 
 # Define and organise buttons
 k = 0
@@ -105,6 +123,7 @@ curampl_prompt_lbl.grid(row=0,column=0,columnspan=2, sticky="nsew")
 onoff_prompt_lbl.grid(row=0,column=0)
 set_current_lvl_box.grid(row=1,column=0)
 mA_lbl.grid(row=1,column=1)
+serial_mon_lbl.grid(row=0,column=0)
 #set_current_lvl_box.insert(0,"Input current level")
 
 set_current_lvl = set_current_lvl_box.get()
